8a769e11c824e78c89fcc3044adf7460dbb531bb
Sebastian Olivares <olivares.heredia@gmail.com>
2025-05-29 12:39:48 +0200

    my first component

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,18 @@ import { createRoot } from "react-dom/client"
 
 const root = createRoot(document.querySelector('#root'))
 
+function MyFirstList () {
+  return (
+    <ul>
+      <li>Become a competent dev</li>
+      <li>Build out of the ordinary web apps</li>
+      <li>Get paid while at it</li>
+    </ul>
+  )
+}
+
 root.render(
-  <ul>
-    <li>Become a competent dev</li>
-    <li>Build out of the ordinary web apps</li>
-    <li>Get paid while at it</li>
-  </ul>
+  <div>
+    <MyFirstList />
+  </div>
 )
