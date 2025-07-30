c02b7b4ca52b4de39c799d591dd61aa8e8c91f1d
Sebastian Olivares <olivares.heredia@gmail.com>
2025-07-30 13:41:11 +0200

    travel journey header ready

---
+++ src/TravelJourney/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header>
+      <img src="globe.png" alt="globe-logo" />
+      <h1>My Travel Journal</h1>
+    </header>
+  )
+}
---
+++ src/TravelJourney/index.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client"
+import MyJourney from "./MyJourney"
+
+const root = createRoot(document.querySelector('#root'))
+
+root.render(
+  <MyJourney />
+)
