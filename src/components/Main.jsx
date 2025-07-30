10eca2c1954ccf0d5691c49bdbcaaa80ae22ee29
Sebastian Olivares <olivares.heredia@gmail.com>
2025-07-30 12:28:40 +0200

    styled navbar and main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,14 @@
 export default function Main() {
   return (
-    <h1>Main goes here</h1>
+    <main>
+      <h1>Fun facts about React</h1>
+      <ul className="facts-list">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>has well over 200k stars on GitHub</li>
+        <li>Is mantained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
   )
 }
