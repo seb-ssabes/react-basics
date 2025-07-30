a5392e30e4491ff2a508a70e618df0abdcf3d462
Sebastian Olivares <olivares.heredia@gmail.com>
2025-07-30 10:54:50 +0200

    first custom components

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,51 @@ import { createRoot } from "react-dom/client"
 const root = createRoot(document.querySelector('#root'))
 
 root.render(
-  <TemporaryName />
-
+  <Page />
 )
 
-function TemporaryName() {
+function Page() {
+  return (
+    <div className="page-wrapper">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  )
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <img src="/src/assets/react.svg" className="react-logo" alt="React logo" />
+      <nav>
+        <ul className="nav-list">
+          <li className="nav-list-item">Pricing</li>
+          <li className="nav-list-item">About</li>
+          <li className="nav-list-item">Contact</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+function Main() {
   return (
     <main>
-      <img src="/src/assets/react.svg" alt="" />
       <h1>My list</h1>
-      <ul>
-        <li>Become a competent dev</li>
+      <ol>
+        <li>Become a dev</li>
         <li>Build out of the ordinary web apps</li>
         <li>Get paid while at it</li>
-      </ul>
+      </ol>
     </main>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      <small>© 2025 Olivares development. All rights reserved.</small>
+    </footer>
   )
 }
