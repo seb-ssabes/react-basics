46a12e8e24c2f89b406b8e0f1e28a86a916e13bc
Sebastian Olivares <olivares.heredia@gmail.com>
2025-07-31 16:17:15 +0200

    first prop entry in MyJourney mini project

--- src/TravelJourney/index.jsx
+++ src/TravelJourney/index.jsx
@@ -1,27 +1,12 @@
-// import { createRoot } from "react-dom/client"
-// import MyJourney from "./MyJourney"
-// import Entry from "./components/Entry"
+import { createRoot } from "react-dom/client"
+import MyJourney from "./MyJourney"
+import Entry from "./components/Entry"
 
-import ReactDOM from "react-dom/client"
-
-// const root = createRoot(document.querySelector('#root')).render(<Name />)
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const root = createRoot(document.querySelector('#root'))
 
 
-// root.render(
-//   <>
-//     <MyJourney />
-//   </>
-// )
-
-
-function App() {
-  const hours = new Date().getHours() % 12
-  const minutes = new Date().getMinutes()
-
-
-  return (
-    <h1>It is currently around {hours} and {minutes}</h1>
-  )
-}
+root.render(
+  <>
+    <MyJourney />
+  </>
+)
