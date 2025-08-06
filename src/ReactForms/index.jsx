6d16421833d301c8e22641076c7013aa18a7251f
Sebastian Olivares <olivares.heredia@gmail.com>
2025-08-06 12:06:13 +0200

    React forms basic set up

---
+++ src/ReactForms/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+
+function App() {
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    const formEl = event.currentTarget
+    const formData = new FormData(formEl)
+    const email = formData.get("email")
+    console.log(email)
+    formEl.reset()
+  }
+
+  return (
+    <section>
+      <h1>Signup form</h1>
+      <form onSubmit={handleSubmit} method="post">
+        <label htmlFor="email">Email:</label>
+        <input id="email" type="email" name="email" placeholder="joe@schmoe.com" />
+        <br />
+
+        <label htmlFor="password">Password:</label>
+        <input id="password" type="password" name="password"/>
+        <br />
+
+        <button>Submit</button>
+
+      </form>
+
+    </section>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
