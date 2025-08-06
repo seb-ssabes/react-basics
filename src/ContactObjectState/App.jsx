5b6754ab8d05a7928efe82990d801b967941512c
Sebastian Olivares <olivares.heredia@gmail.com>
2025-08-06 11:04:19 +0200

    contact miniproject set up for useState on an object

---
+++ src/ContactObjectState/App.jsx
@@ -0,0 +1,56 @@
+import React from "react"
+import avatar from "./images/user.png"
+import starFilled from "./images/star-filled.png"
+import starEmpty from "./images/star-empty.png"
+
+export default function App() {
+    const [contact, setContact] = React.useState({
+        firstName: "John",
+        lastName: "Doe",
+        phone: "+1 (212) 555-1212",
+        email: "itsmyrealname@example.com",
+        isFavorite: true
+    })
+
+    let starIcon = contact.isFavorite ? starFilled : starEmpty
+    let favoriteLabel = contact.isFavorite ? "Remove from favorites" : "Add to favorites"
+    let altStarIcon = contact.isFavorite ? "filled star icon" : "empty star icon"
+
+    function toggleFavorite() {
+      setContact(
+        setContact.isFavorite = !setContact.isFavorite
+      )
+    }
+
+    return (
+        <main>
+            <article className="card">
+                <img
+                    src={avatar}
+                    className="avatar"
+                    alt="User profile picture of John Doe"
+                />
+                <div className="info">
+                    <button
+                        onClick={toggleFavorite}
+                        aria-pressed={contact.isFavorite}
+                        aria-label={favoriteLabel}
+                        className="favorite-button"
+                    >
+                        <img
+                            src={starIcon}
+                            alt={altStarIcon}
+                            className="favorite"
+                        />
+                    </button>
+                    <h2 className="name">
+                        {contact.firstName} {contact.lastName}
+                    </h2>
+                    <p className="contact">{contact.phone}</p>
+                    <p className="contact">{contact.email}</p>
+                </div>
+
+            </article>
+        </main>
+    )
+}
