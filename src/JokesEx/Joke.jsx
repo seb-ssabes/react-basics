8ca62ab20aad61768f5ca34ea909f6ffd09bbbc0
Sebastian Olivares <olivares.heredia@gmail.com>
2025-08-10 20:09:33 +0200

    intro to conditional rendering

--- src/JokesEx/Joke.jsx
+++ src/JokesEx/Joke.jsx
@@ -1,8 +1,19 @@
+import React from "react"
+
 export default function Joke(props) {
+  const [isShown, setIsShown] = React.useState(false)
+
+  function togglePunchline() {
+    setIsShown(prevIsShown => !prevIsShown)
+  }
+
+  console.log(isShown)
+
   return (
     <>
-      {props.setup && <p>Setup: {props.setup}</p>}
-      <p>Punchline: {props.punchline}</p>
+      {props.setup && <h3>{props.setup}</h3>}
+      {isShown && <p>{props.punchline}</p>}
+      <button onClick={togglePunchline}>Show Punchilne</button>
       <hr />
     </>
   )
