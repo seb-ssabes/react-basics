4bd09addeff38ed92f76fb1c61d02955247c5639
Sebastian Olivares <olivares.heredia@gmail.com>
2025-08-13 12:12:41 +0200

    Pad onClick event working with useState

--- src/PadsStateProps/Pad.jsx
+++ src/PadsStateProps/Pad.jsx
@@ -1,8 +1,18 @@
+import React from "react"
+
 export default function Pad(props) {
+  const [padStatus, setPadStatus] = React.useState(props.on)
+
+  function padToggle() {
+    console.log(padStatus)
+    setPadStatus(prevPad => (!prevPad))
+  }
+
   return (
     <button
       style={{backgroundColor: props.color}}
-      className={props.on ? "on" : undefined}
+      className={padStatus ? "on" : undefined}
+      onClick={padToggle}
     >
     </button>
   )
