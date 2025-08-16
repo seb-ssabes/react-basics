aeb1c9b9fd0b620c9b4ba472af5d2e0ad7eaff95
Sebastian Olivares <olivares.heredia@gmail.com>
2025-08-16 10:22:59 +0200

    useEffect working with dependency

--- src/FetchDataStarWars/App.jsx
+++ src/FetchDataStarWars/App.jsx
@@ -1,19 +1,22 @@
 import React from "react"
 
 export default function App(props) {
-  const [starWarsData, setStarWarsData] = React.useState(null)
+  const [starWarsData, setStarWarsData] = React.useState({})
+  const [count, setCount] = React.useState(0)
+
+  console.log("Rendered")
 
   React.useEffect(function() {
+    console.log("Effect ran")
     fetch("https://swapi.py4e.com/api/people/1")
         .then(res => res.json())
-        // .then(data => setStarWarsData(data))
-  })
-
-
-
+        .then(data => setStarWarsData(data))
+  }, [count])
 
   return (
       <div>
+          <h2>The count is {count}</h2>
+          <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
           <pre>{JSON.stringify({starWarsData}, null, 2)}</pre>
       </div>
   )
