80df49368d57f0bf42030744720210b32181398c
Sebastian Olivares <olivares.heredia@gmail.com>
2025-08-19 11:39:34 +0200

    useEffect updated with dinamic API

--- src/FetchDataStarWars/App.jsx
+++ src/FetchDataStarWars/App.jsx
@@ -1,23 +1,23 @@
 import React from "react"
 
-export default function App(props) {
+export default function App() {
   const [starWarsData, setStarWarsData] = React.useState({})
-  const [count, setCount] = React.useState(0)
+  const [count, setCount] = React.useState(1)
 
   console.log("Rendered")
 
   React.useEffect(function() {
-    console.log("Effect ran")
-    fetch("https://swapi.py4e.com/api/people/1")
-        .then(res => res.json())
-        .then(data => setStarWarsData(data))
+    console.log("Effect ran!")
+    fetch(`https://swapi.py4e.com/api/people/${count}`)
+      .then(res => res.json())
+      .then(data => setStarWarsData(data))
   }, [count])
 
   return (
       <div>
           <h2>The count is {count}</h2>
-          <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
-          <pre>{JSON.stringify({starWarsData}, null, 2)}</pre>
+          <button onClick={() => setCount(prevCount => prevCount + 1)}>Get next character</button>
+          <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
       </div>
   )
 }
