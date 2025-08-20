7eee303541bfbb6660600fbe3bdf64602fba6da1
Sebastian Olivares <olivares.heredia@gmail.com>
2025-08-20 07:57:35 +0200

    Added 10 buttons with random numbers on refresh

--- src/Tenzies/App.jsx
+++ src/Tenzies/App.jsx
@@ -1,20 +1,20 @@
 import Die from "./Die"
+import React from "react"
 
 export default function App() {
+  const [dice, setdice] = React.useState(generateAllNewDice())
+
+  function generateAllNewDice() {
+    return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6))
+  }
+
+  const diceElements = dice.map(dice => <Die value={dice}/>)
+
   return (
     <main>
       <div className="dice-container">
 
-        <Die value={1}/>
-        <Die value={2}/>
-        <Die value={3}/>
-        <Die value={4}/>
-        <Die value={5}/>
-        <Die value={6}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
+        {diceElements}
 
       </div>
     </main>
