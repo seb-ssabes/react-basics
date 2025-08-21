0cc5af316594b220699d1846107452c15c3f1a7c
Sebastian Olivares <olivares.heredia@gmail.com>
2025-08-21 10:43:22 +0200

    Assembly:Endgame project set up

---
+++ src/AssemblyEndgame/App.jsx
@@ -0,0 +1,10 @@
+export default function AssemblyEndgame() {
+  return (
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+      </header>
+    </main>
+  )
+}
--- src/Tenzies/Die.jsx
+++ src/Tenzies/Die.jsx
@@ -8,3 +8,4 @@ export default function Die(props) {
     >{props.value}</button>
   )
 }
+   
