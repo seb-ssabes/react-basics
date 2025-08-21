b747d60a9e5392a0d1c660bc7c47a08ca00791c8
Sebastian Olivares <olivares.heredia@gmail.com>
2025-08-21 11:38:19 +0200

    added header, game status and languages sections

--- src/AssemblyEndgame/App.jsx
+++ src/AssemblyEndgame/App.jsx
@@ -1,10 +1,37 @@
+import React from "react"
+import {languages} from "./languages.js"
+
 export default function AssemblyEndgame() {
+  const languageElements = languages.map(lang => {
+    const styles = {
+      backgroundColor: lang.backgroundColor,
+      color: lang.color,
+    }
+
+    return (
+      <span
+        key={lang.name}
+        className="chip"
+        style={styles}
+      >
+        {lang.name}
+      </span>
+    )
+  })
+
   return (
     <main>
       <header>
         <h1>Assembly: Endgame</h1>
         <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
       </header>
+      <section className="game-status">
+        <h2>You win!</h2>
+        <p>Well done!</p>
+      </section>
+      <section className="language-chips">
+        {languageElements}
+      </section>
     </main>
   )
 }
